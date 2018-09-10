b71f435fd54e25311532725dc60b2df9ee78d957
strongSoda <strongsoda2@gmail.com>
2018-09-10 21:52:11 +0530

    Added functionality to add new products via Admin Dashboard GUI. There's a bug though

--- controllers/manage.js
+++ controllers/manage.js
@@ -1,13 +1,79 @@
 'use strict';
 
+var Product = require('../models/productModel');
+var Category = require('../models/categoryModel');
+
 module.exports = function(router){
     router.get('/',function(req,res){
         res.render('manage/index');
     });
 
     router.get('/products',function(req,res){
-        res.render('manage/products/index');
+        Product.find({}, function(err, products){
+            if(err) {
+                console.log(err); 
+            }
+  
+            var model = {
+                products: products
+            }
+  
+            res.render('manage/products/index', model);
+        });     
     });
+
+    router.get('/products/add',function(req,res){
+        Category.find({},function(err,categories){
+            if(err) {
+                console.log(err);
+            }
+            var model = {
+                categories: categories
+            }
+
+            res.render('manage/products/add', model);
+        });
+
+    });
+
+    router.post('/products' , function(req,res) {
+        var title = req.body.title && req.body.title.trim();
+        var description = req.body.description && req.body.description.trim();
+        var seller = req.body.seller && req.body.seller.trim();
+        var category = req.body.category && req.body.category.trim();
+        var price = req.body.price && req.body.price.trim();
+        var thumbnail = req.body.thumbnail && req.body.thumbnail.trim();
+        var hero = req.body.hero && req.body.hero.trim();
+        var warranty = req.body.warranty && req.body.warranty.trim();
+
+        if(title == '' || price == '' || seller == '' || hero == '' || thumbnail == '' ) {
+            req.flash('error', "Please fill out the required fields");
+            res.location('/manage/products/add');
+            res.redirect('/manage/products/add');
+        }
+
+        var newProduct = new Product({
+            title: title,
+            category: category,
+            description: description,
+            warranty: warranty,
+            seller: seller,
+            price: price,
+            hero: hero,
+            thumbnail: thumbnail
+        });
+
+        newProduct.save(function(err) {
+            if(err) {
+                console.log('save error', err);
+            }
+
+            req.flash('success',"Product Added");
+            res.location('/manage/products');
+            res.redirect('/manage/products');
+        })
+    });
+
     router.get('/categories',function(req,res){
         res.render('manage/categories/index');
     });
